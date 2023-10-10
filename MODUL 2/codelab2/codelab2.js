1caa2804aacd6d4d060560d8060fa9340fd999fd
Hendaaaaa23 <127113051+Hendaaaaa23@users.noreply.github.com>
2023-10-10 13:58:33 +0700

    Add files via upload

---
+++ MODUL 2/codelab2/codelab2.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const buttonDaftar = document.querySelector(".custom-btn");
+
+  buttonDaftar.addEventListener("click", function () {
+    const nama = document.getElementById("input1").value;
+    const email = document.getElementById("input2").value;
+    const alamat = document.getElementById("input3").value;
+
+    if (nama === "" || email === "" || alamat === "") {
+      alert("Anda harus mengisi data dengan lengkap!");
+    }
+  });
+});
